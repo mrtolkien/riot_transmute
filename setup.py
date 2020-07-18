0c46f870ddbd6fed06d91115e73773f20e98b75b
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-07-19 00:17:06 +0900

    Setup update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="riot_transmute",
-    version="0.1a16",
+    version="0.1a18",
     packages=setuptools.find_packages(),
     install_requires=["lol_dto >= 0.1a4"],
     extra_require={"names": ["lol_id_tools"]},
