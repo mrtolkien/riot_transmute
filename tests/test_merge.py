f01ba56061f01a888de7c55ca24ea2464b5657a3
Tolki <gary.mialaret@gmail.com>
2021-07-09 16:07:03 +0900

    Test refactor

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,10 +1,9 @@
 import json
 import os
-
-import lol_dto.utilities
 import roleml
 
 from riot_transmute import match_to_game, match_timeline_to_game
+from riot_transmute.merge_match_and_timeline import merge_games_from_riot_match_and_timeline
 
 
 def test_full(match_dto, timeline_game_id_platform_id):
@@ -19,7 +18,7 @@ def test_full(match_dto, timeline_game_id_platform_id):
     game_match = match_to_game(match_dto)
     game_timeline = match_timeline_to_game(timeline, game_id, platform_id)
 
-    game_full = lol_dto.utilities.merge_games(game_match, game_timeline)
+    game_full = merge_games_from_riot_match_and_timeline(game_match, game_timeline)
 
     with open(os.path.join("examples", "game_merged.json"), "w+") as file:
         json.dump(game_full, file, indent=4)
@@ -36,8 +35,8 @@ def test_custom_game(watcher):
     match = watcher.match.by_id("EUW1", 4676184349)
     timeline = watcher.match.timeline_by_match("EUW1", 4676184349)
 
-    game = match_to_game(match, True)
-    game_timeline = match_timeline_to_game(timeline, 4676184349, "EUW1", True)
+    game = match_to_game(match)
+    game_timeline = match_timeline_to_game(timeline, 4676184349, "EUW1")
 
-    # TODO This should not be there anymore
-    assert lol_dto.utilities.merge_games(game, game_timeline)
+    # TODO Save test file sources
+    assert merge_games_from_riot_match_and_timeline(game, game_timeline)
