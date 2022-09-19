fc6563a0c4ee959c533dfa666b735d57e678229b
mrtolkien <gary.mialaret@gmail.com>
2022-09-19 15:44:29 +0900

    Conftest rewrite

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,58 +1,46 @@
+"""Pytest defaults.
+"""
+
 import json
 import os
 
-import pytest
 import dotenv
+import pytest
 
 dotenv.load_dotenv()
 
 
-@pytest.fixture
-def match_v4_dto():
-    file_location = os.path.join("tests", "data", "v4", "source_match.json")
+def load_game(*args) -> dict:
+    file_location = os.path.join(*args)
 
-    with open(file_location) as file:
+    with open(file_location, encoding="utf-8") as file:
         match_dto = json.load(file)
 
     return match_dto
+
+
+@pytest.fixture
+def match_v4_dto():
+    return load_game("tests", "data", "v4", "source_match.json")
 
 
 @pytest.fixture
 def timeline_game_id_platform_id():
-    file_location = os.path.join("tests", "data", "v4", "source_timeline.json")
-
-    with open(file_location) as file:
-        timeline_game_platform = json.load(file)
-
-    return timeline_game_platform, 4409190456, "KR"
+    return load_game("tests", "data", "v4", "source_timeline.json"), 4409190456, "KR"
 
 
 @pytest.fixture
 def esports_match_dto():
-    with open(os.path.join("tests", "data", "v4", "source_match_esports.json")) as file:
-        match_dto = json.load(file)
-
-    return match_dto
+    return load_game("tests", "data", "v4", "source_match_esports.json")
 
 
 @pytest.fixture()
 def esports_timeline():
-    with open(
-        os.path.join("tests", "data", "v4", "source_timeline_esports.json")
-    ) as file:
-        match_dto = json.load(file)
-
-    return match_dto
+    load_game("tests", "data", "v4", "source_timeline_esports.json")
 
 
 @pytest.fixture
 def custom_match_and_timeline():
-    with open(os.path.join("tests", "data", "v4", "source_custom.json")) as file:
-        match = json.load(file)
-
-    with open(
-        os.path.join("tests", "data", "v4", "source_custom_timeline.json")
-    ) as file:
-        timeline = json.load(file)
-
-    return match, timeline
+    return load_game("tests", "data", "v4", "source_custom.json"), load_game(
+        "tests", "data", "v4", "source_custom_timeline.json"
+    )
