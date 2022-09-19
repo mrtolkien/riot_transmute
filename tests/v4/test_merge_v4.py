9b8914042c4725257a1e50a0270bb0bc005a24e9
mrtolkien <gary.mialaret@gmail.com>
2022-09-19 15:51:42 +0900

    v4 cleanup

---
+++ tests/v4/test_merge_v4.py
@@ -0,0 +1,45 @@
+"""Testing merging of match + timeline.
+"""
+
+import os
+
+import lol_dto
+
+from riot_transmute import match_timeline_to_game, match_to_game
+from riot_transmute.merge_match_and_timeline import (
+    merge_games_from_riot_match_and_timeline,
+)
+
+
+def test_full(match_v4_dto, timeline_game_id_platform_id):
+    timeline, game_id, platform_id = timeline_game_id_platform_id
+
+    # TODO Re-check what that did and removing it would be a problem
+    # match_dto, timeline = roleml.fix_game(
+    #     match_v4_dto,
+    #     timeline,
+    #     True,
+    # )
+
+    game_match = match_to_game(match_v4_dto)
+    game_timeline = match_timeline_to_game(timeline, game_id, platform_id)
+
+    game_full = merge_games_from_riot_match_and_timeline(game_match, game_timeline)
+
+    lol_dto.utilities.dump_json(game_full, os.path.join("examples", "game_merged.json"))
+
+    assert game_full.sources.riotLolApi.gameId == game_id
+    assert len(game_full.teams.BLUE.players[0].snapshots) > 0
+    assert game_full.duration
+    assert game_full.patch
+
+    return game_full
+
+
+def test_custom_game(custom_match_and_timeline):
+    match, timeline = custom_match_and_timeline
+
+    game = match_to_game(match)
+    game_timeline = match_timeline_to_game(timeline, 4676184349, "EUW1")
+
+    assert merge_games_from_riot_match_and_timeline(game, game_timeline)
