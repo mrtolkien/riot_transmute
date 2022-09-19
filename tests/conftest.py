fc81a1fa3af7978efe9e62c6d07f0d36003cd0e9
mrtolkien <gary.mialaret@gmail.com>
2022-09-19 17:11:37 +0900

    Conftest syntax cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ dotenv.load_dotenv()
 
 
 def load_game(*args) -> dict:
-    file_location = os.path.join(*args)
+    file_location = os.path.join("tests", "data", *args)
 
     with open(file_location, encoding="utf-8") as file:
         match_dto = json.load(file)
@@ -21,26 +21,26 @@ def load_game(*args) -> dict:
 
 @pytest.fixture
 def match_v4_dto():
-    return load_game("tests", "data", "v4", "source_match.json")
+    return load_game("v4", "source_match.json")
 
 
 @pytest.fixture
 def timeline_game_id_platform_id():
-    return load_game("tests", "data", "v4", "source_timeline.json"), 4409190456, "KR"
+    return load_game("v4", "source_timeline.json"), 4409190456, "KR"
 
 
 @pytest.fixture
 def esports_match_dto():
-    return load_game("tests", "data", "v4", "source_match_esports.json")
+    return load_game("v4", "source_match_esports.json")
 
 
 @pytest.fixture()
 def esports_timeline():
-    return load_game("tests", "data", "v4", "source_timeline_esports.json")
+    return load_game("v4", "source_timeline_esports.json")
 
 
 @pytest.fixture
 def custom_match_and_timeline():
-    return load_game("tests", "data", "v4", "source_custom.json"), load_game(
-        "tests", "data", "v4", "source_custom_timeline.json"
+    return load_game("v4", "source_custom.json"), load_game(
+        "v4", "source_custom_timeline.json"
     )
